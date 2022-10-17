f4125ce4d9a2a6cbd9ee0cac62b53b0453ae01a6
Seongjun Min <cazador7@naver.com>
2022-10-18 01:42:51 +0900

    1018

---
+++ BOJ/단계별/재귀/팩토리얼,10872.js
@@ -0,0 +1,11 @@
+let fs = require("fs");
+let input = fs.readFileSync("input.txt").toString().split("\n");
+function solution(n) {
+    if (n === 1) {
+        return 1;
+    }
+
+    return n * solution(n - 1);
+}
+
+console.log(solution(input));
--- boj_template.js
+++ boj_template.js
@@ -1,7 +1,10 @@
+// 로컬에서
 let fs = require("fs");
 let input = fs.readFileSync("input.txt").toString().split("\n");
+
+// 백준제출시
 // let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-let n = Number(input[0]);
+// let n = Number(input[0]);
 
 
